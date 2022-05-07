fe4dde837ab511c7d671b0a38905f2c29ad37d57
sdonne <stefanodonne@gmail.com>
2022-05-07 16:02:03 +0200

    GO LEFT

--- behavior_tree_pacman.py
+++ behavior_tree_pacman.py
@@ -1,9 +1,19 @@
+from arena import *
+from pacman import *
+
 def action_from_state(obs):
     """
     :param obs: current game state (arena.actors, list)
     :return:
     """
+
+    # example
+    ghosts = []
+    for elem in obs:
+        if isinstance(elem, Ghost):
+            ghosts.append(elem)
     action_dx, action_dy = -2, 0  # move pacman up, temp for test
+    #
 
     ###
     # PROCESS THE GAME STATE HERE , RETURN ACTION TO DO FOR PACMAN
