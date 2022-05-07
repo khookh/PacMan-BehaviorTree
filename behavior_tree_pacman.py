199c53db32ae118aa534aac9ce622075316e4fb8
sdonne <stefanodonne@gmail.com>
2022-05-07 15:47:21 +0200

    Modify pacman movement interface

---
+++ behavior_tree_pacman.py
@@ -0,0 +1,12 @@
+def action_from_state(obs):
+    """
+    :param obs: current game state (arena.actors, list)
+    :return:
+    """
+    action = (-2, 0)  # move pacman up, temp for test
+
+    ###
+    # PROCESS THE GAME STATE HERE , RETURN ACTION TO DO FOR PACMAN
+    ###
+
+    return action
