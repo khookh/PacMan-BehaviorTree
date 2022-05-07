ccbf8c6b5761af5e0cd76f0ed06f1686a95102bc
sdonne <stefanodonne@gmail.com>
2022-05-07 15:55:22 +0200

    GO LEFT

--- behavior_tree_pacman.py
+++ behavior_tree_pacman.py
@@ -3,7 +3,7 @@ def action_from_state(obs):
     :param obs: current game state (arena.actors, list)
     :return:
     """
-    action_dx, action_dy = 0, 2  # move pacman up, temp for test
+    action_dx, action_dy = -2, 0  # move pacman up, temp for test
 
     ###
     # PROCESS THE GAME STATE HERE , RETURN ACTION TO DO FOR PACMAN
